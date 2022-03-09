166b1057dec4383e69c99c1a6e39526896e2c215
Dalia <dalia.a.95@gmail.com>
2022-03-09 00:51:02 -0500

    added index, css, and js page. also added basic calculator functions

---
+++ script.js
@@ -0,0 +1,42 @@
+
+const add = function (num1, num2) {
+    const total = num1 + num2;
+    return total;
+}
+
+console.log(add(5, 7));
+
+const subtract = function (num1, num2) {
+    const total = num1 - num2;
+    return total;
+}
+
+console.log(subtract(5, 7));
+
+const divide = function (num1, num2) {
+    const total = num1 / num2;
+    return total;
+}
+
+console.log(divide(5, 7));
+
+const multiply = function (num1, num2) {
+    const total = num1 * num2;
+    return total;
+}
+
+console.log(multiply(5, 7));
+
+const operate = function (operator, num1, num2) {
+    if (operator === 'add') {
+        return add(num1, num2);
+    } else if (operator === 'subtract') {
+        return subtract(num1, num2);
+    } else if (operator === 'divide') {
+        return divide(num1, num2);
+    } else if (operator === 'multiply') {
+        return multiply(num1,num2);
+    } else return "not a valid response";
+};
+
+console.log(operate('mult', 5, 8));
