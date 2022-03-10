05440129ea142e581da0d5d765d8cb06eac125b3
Dalia <dalia.a.95@gmail.com>
2022-03-09 22:11:12 -0500

    divide and equal function added

--- script.js
+++ script.js
@@ -1,51 +1,55 @@
 const numBtn = document.querySelectorAll('.num-btn');
 const display = document.getElementById('display');
+const divideBtn = document.getElementById('divide-btn');
+const equalBtn = document.getElementById('equal-btn');
 
+const displayValue = display.innerHTML;
+
+//display number after clicked
 numBtn.forEach(num => {
     num.addEventListener('click', function clickNum() {
         display.innerHTML += num.innerHTML;
     });
 })
+let num1 = [];
+let num2 = [];
 
-//clicking on number displays number
-//function clickNum (){
- //   Array.from(numBtn).forEach(function (num) {
-//        let numDisplay = display.innerHTML
-//        num.onclick = function() {
- //           display.innerHTML += num.innerHTML;
- //       }
- //   })
-//};
-//clickNum();
+//stores first nums set when clicking divide btn
+function divideNums() {
+    num1.push(display.innerHTML);
+    display.innerHTML = "";
+    console.log(num1)
+}
+divideBtn.addEventListener('click', divideNums);
+
+function equals () {
+    num2.push(display.innerHTML);
+    console.log(num2);
+    total = num1 / num2;
+    display.innerHTML = total;
+}
+equalBtn.addEventListener('click', equals);
 
 
 const add = function (num1, num2) {
     const total = num1 + num2;
     return total;
-}
-
-console.log(add(5, 7));
+};
 
 const subtract = function (num1, num2) {
     const total = num1 - num2;
     return total;
-}
-
-console.log(subtract(5, 7));
+};
 
 const divide = function (num1, num2) {
     const total = num1 / num2;
     return total;
-}
-
-console.log(divide(5, 7));
+};
 
 const multiply = function (num1, num2) {
     const total = num1 * num2;
     return total;
-}
-
-console.log(multiply(5, 7));
+};
 
 
 const operate = function (operator, num1, num2) {
